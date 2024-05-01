584e5655c6bec006e70e5ed3b4bbc7c5ae3aeecc
SathishKumar03082004 <sathishkumar03082004@gmail.com>
2024-05-01 18:05:04 +0530

    FOV

--- FieldofView.cs
+++ FieldofView.cs
@@ -22,3 +22,22 @@ public class VirtualRealityCamera
         Console.WriteLine("Field of View: " + fov.ToString("0.00") + " degrees");
     }
 }
+
+
+/*
+The FOV (θ) can be calculated using the Formula:
+
+PI=2*arctan(s/2*f)
+
+
+focal length (f): The distance from the lens to the camera sensor.
+sensor size (s): The size of the camera sensor.
+
+
+we divide the sensor size by 2 X f to get the tangent of half the FOV angle, and then take the arctan to get the angle itself.
+Since the FOV is typically given in degrees, you might want to convert the result from radians to degrees using the conversion factor 180/PI.
+
+
+This formula calculates the FOV horizontally. If you need the FOV vertically, you can use the same formula but with the vertical sensor size instead.
+If your camera's sensor is not square, you might need separate formulas for the horizontal and vertical FOV.
+*/
